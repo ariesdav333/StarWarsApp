75c1ddef659857f5d2528611a9bb3096219ce6c1
david <davidwilliams_canada@yahoo.com>
2019-03-09 18:34:53 +0000

    Added a header title and subheading along with some data from the API for planets. I added some different components to te project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
     <h1 class="header"> Star Wars </h1>
+
+    <h2 class="header-h2"> Below are a list of Planets with some information about them </h2>
     <div class="container-fluid">
       <div class="columns"> 
       <div v-for="(planet, index) in planets" :key="index">
-        <pre>{{planets.name}}</pre>
+       <!-- <pre>{{planets}}</pre> -->
         <div class="column">
-        {{planet.name}}
+        <b>Name:</b>{{planet.name}}
+        <p><b>Rotation:</b>{{planet.rotation_period}}</p>
+        <p><b>Usual Climate:</b> {{planet.climate}}</p>
         </div>
       </div>
       </div>
@@ -57,6 +61,10 @@ export default {
   padding: 2px;
 }
 .header{
+  padding: 20px;
+  font-size: 45px;
+}
+.header-h2{
   padding: 20px;
 }
 
