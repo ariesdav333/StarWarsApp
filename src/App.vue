569760da5e393682cc1a68aa259ee387f49a4c7b
david <davidwilliams_canada@yahoo.com>
2019-03-09 19:04:33 +0000

    Added a blacknav bar to stick with the starwars theme. Added some routes into the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,38 @@
 <template>
   <div id="app">
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class=""> <b>Star Wars</b> </a>
+        </div>
+
+          <span class="nav-toggle">  <!-- for mobile devices -->
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+
+        <div class="nav-right nav-menu"> <!-- Create the router links here -->
+
+      <router-link to="planets" class="nav-item"> Planets </router-link>
+      <router-link to="films" class="nav-item r-item"> Films </router-link>
+      <router-link to="people" class="nav-item r-item"> People </router-link>
+       <router-link to="species" class="nav-item r-item"> Species </router-link>
+        <router-link to="starships" class="nav-item r-item"> StarShips </router-link>
+         <router-link to="vehicles" class="nav-item r-item"> Vehicles </router-link>
+
+      <div class="nav-item"> 
+        <p class="control">
+
+        </p>
+
+
+      </div>
+        </div>
+      </div>
+    </div>
+
+
     <h1 class="header"> Star Wars </h1>
 
     <h2 class="header-h2"> Below are a list of Planets with some information about them </h2>
@@ -22,6 +55,8 @@
 </template>
 
 <script>
+
+
 export default {
 
   data(){
@@ -66,6 +101,10 @@ export default {
 }
 .header-h2{
   padding: 20px;
+}
+
+.nav{
+  background-color: black;
 }
 
 </style>
